928e5654e667743fe3cafe5423e06b647dd975d4
iska <iska.gp@gmail.com>
2019-07-09 22:36:42 +0200

    Fix JSON deserializing for Analysis class

--- src/main/java/iabudiab/maven/plugins/dependencytrack/client/model/Analysis.java
+++ src/main/java/iabudiab/maven/plugins/dependencytrack/client/model/Analysis.java
@@ -1,5 +1,7 @@
 package iabudiab.maven.plugins.dependencytrack.client.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -7,5 +9,6 @@ public class Analysis {
 
 	private State state;
 
-	private boolean isSuppressed;
+	@JsonProperty("isSuppressed")
+	private boolean suppressed;
 }
