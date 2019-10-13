0e3a844a546c8e43d4133dd872b6a8f5dfce7a51
iska <iska.gp@gmail.com>
2019-10-13 23:15:14 +0200

    Fix security gate findings checks

--- src/main/java/iabudiab/maven/plugins/dependencytrack/SecurityGate.java
+++ src/main/java/iabudiab/maven/plugins/dependencytrack/SecurityGate.java
@@ -19,10 +19,10 @@ public class SecurityGate {
 	}
 
 	public void applyOn(ProjectMetrics projectMetrics) throws SecurityGateRejectionException {
-		if (projectMetrics.getCritical() >= critical || //
-				projectMetrics.getHigh() >= high || //
-				projectMetrics.getMedium() >= medium || //
-				projectMetrics.getLow() >= low) {
+		if (projectMetrics.getCritical() > critical || //
+				projectMetrics.getHigh() > high || //
+				projectMetrics.getMedium() > medium || //
+				projectMetrics.getLow() > low) {
 			throw new SecurityGateRejectionException("Project did not pass the Security Gate");
 		}
 	}
